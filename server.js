17940968d9092c8863551442ac4d3db446c12ad5
tejas <tejas@metricoidtech.com>
2021-07-19 09:18:12 +0530

    first commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // connect Database
 connectDB();
 
-// init middleleware 
+// init middleleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 // define routes
-app.use('/api/user', require('./routes/api/user'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
